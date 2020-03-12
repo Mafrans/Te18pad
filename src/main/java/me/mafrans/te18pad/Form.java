7d7d190dfe19fe7396a0a6a679db64c419d7bfdc
Mafrans <malte.kluft@gmail.com>
2020-03-12 10:57:05 +0100

    Create simple form

--- src/main/java/me/mafrans/te18pad/Form.java
+++ src/main/java/me/mafrans/te18pad/Form.java
@@ -1,4 +1,12 @@
 package me.mafrans.te18pad;
 
+import javax.swing.*;
+
 public class Form {
+    private JPanel mainPanel;
+    private JSpinner seedSpinner;
+    private JSlider seedSlider;
+    private JTextPane textPane;
+    private JScrollPane scrollPane;
+    private JPanel seedPanel;
 }
