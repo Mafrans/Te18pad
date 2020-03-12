dc1c7c13ab9d7f993f8e2a2f7c79ec270c088687
Mafrans <malte.kluft@gmail.com>
2020-03-12 11:16:10 +0100

    Add Lombok dependency

--- src/main/java/me/mafrans/te18pad/NoteGUI.java
+++ src/main/java/me/mafrans/te18pad/NoteGUI.java
@@ -1,7 +1,11 @@
 package me.mafrans.te18pad;
 
+import lombok.Data;
+import lombok.SneakyThrows;
+
 import javax.swing.*;
 
+@Data
 public class NoteGUI {
     private JPanel mainPanel;
     private JSpinner seedSpinner;
@@ -10,9 +14,12 @@ public class NoteGUI {
     private JScrollPane scrollPane;
     private JPanel seedPanel;
 
+    @SneakyThrows
     public NoteGUI(String title) {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
         JFrame frame = new JFrame(title);
-        frame.setContentPane(mainPanel);
+        frame.setContentPane(getMainPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
