5a9c343cc55c69f3e7619e38d95b74f4c97546a9
Mafrans <malte.kluft@gmail.com>
2020-03-19 10:32:23 +0100

    Set look and feel for the entire app

--- src/main/java/me/mafrans/te18pad/Te18pad.java
+++ src/main/java/me/mafrans/te18pad/Te18pad.java
@@ -1,7 +1,10 @@
 package me.mafrans.te18pad;
 
+import javax.swing.*;
+
 public class Te18pad {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         NoteGUI noteGUI = new NoteGUI("Real Time Encoder");
     }
 }
